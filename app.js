f00ca3353e157482b362e32a73bf315416a4cbc6
Muhammadizhan <izhanayaz4@gmail.com>
2024-05-21 14:51:58 +0500

    js update

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const username = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const form = document.querySelector("#form");
-// form.addEventListener("summit", function (event) {
-//   event.preventDefault();
-//   console.log(username.value);
-//   console.log(email.value);
-//   console.log(password.value);
-// });
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
